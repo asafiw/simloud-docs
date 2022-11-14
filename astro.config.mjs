f436376f63fb97236f88e8e4186455162fb34001
dospolov <marat.dospolov@gmail.com>
2022-11-15 01:14:13 +0700

    remote rehype-starry-night-copy-collapse

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config'
 import preact from '@astrojs/preact'
 import react from '@astrojs/react'
-import rehypeStarryNightCopyCollapse from './rehypeStarryNightCopyCollapse'
+import rehypeStarryNightCopyCollapse from 'rehype-starry-night-copy-collapse'
 
 // https://astro.build/config
 export default defineConfig({
